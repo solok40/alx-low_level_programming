c7499f134a439b932729287660a4a240102628ad
solok40 <solok40@gmail.com>
2021-08-03 11:26:11 -0700

    0x13-more_singly_linked_lists/

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -27,32 +27,3 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	return (NULL);
 }
-#include <stdlib.h>
-#include "lists.h"
-#include <stdio.h>
-
-/**
-  * get_nodeint_at_index - ...
-  * @head: ...
-  * @index: ...
-  *
-  * Return: ...
-  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{
-	unsigned int count = 0;
-
-	if (head)
-	{
-		while (head)
-		{
-			if (count == index)
-				return (head);
-
-			head = head->next;
-			count++;
-		}
-	}
-
-	return (NULL);
-}
